4d8773f49cbb15ad8bcef50656e31b3909711f7c
doshid20 <79118790+doshid20@users.noreply.github.com>
2021-03-10 14:51:48 -0700

    Created functions for accounts.js
    Created functions for accounts.js

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,12 +1,58 @@
 // Note: Please do not change the name of the functions. The tests use those names to validate your code.
 
-function findAccountById(accounts, id) {}
+function findAccountById(accounts, id) {
 
-function sortAccountsByLastName(accounts) {}
+  const findaccountId = accounts.find((element) => element.id === id);
+  return findaccountId;
+}
 
-function getTotalNumberOfBorrows(account, books) {}
+function sortAccountsByLastName(accounts) {
+  const accountsortedinorder = accounts.sort((accountA, accountB) => 
+  accountA.name.last.toLowerCase() > accountB.name.last.toLowerCase() ? 1 : -1);
+  return accountsortedinorder;
+}
 
-function getBooksPossessedByAccount(account, books, authors) {}
+function getTotalNumberOfBorrows(account, books) {
+
+  let accumulator = 0;
+  
+  const totalNumberofBorrowByAccount = books.forEach((book) => {
+    const borrowbook = book.borrows.forEach((borrow) => {
+
+      if (borrow.id === account.id) {
+         accumulator += 1;
+      }
+    });
+
+  });
+  return accumulator;
+}
+
+function getAuthorById(authors, id) {
+  const author = authors.find((author) => author.id == id);
+  return author;
+}
+
+function getBooksPossessedByAccount(account, books, authors) {
+  let result = [];
+  books.forEach((book) => {
+       const borrowedbook = book.borrows.forEach((borrow) =>  {
+           if(borrow.id === account.id && !borrow.returned) {
+              result.push(book);
+           }
+        });     
+  })
+   result = result.map((book) => {
+    let author =  getAuthorById(authors, book.authorId);
+    
+    const newObjWithAuthor = {
+      ...book,
+      author,
+    }
+    return newObjWithAuthor;
+  });
+  return result;
+}
 
 module.exports = {
   findAccountById,
